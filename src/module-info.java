10ee41bb2d03bd0ae7bd439ead9dffc949274b0c
rjsvcs <rjsvcs@rit.edu>
2020-02-09 09:51:34 -0500

    removed the module path from the run configuration

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module CS2Week10Studio {
+    requires transitive javafx.controls;
+    exports clock;
+    exports fxactivities;
+    exports fxexamples;
+    exports guessing;
+    exports nappers;
+    exports naptimer;
+}
